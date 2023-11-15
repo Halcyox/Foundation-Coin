e8336817c847388a233d1ac7ca57708e3477ee37
Caerii <cubelocked@gmail.com>
2023-11-15 00:01:48 -0500

    copy change

--- foundation-coin/src/components/Hero/Hero.jsx
+++ foundation-coin/src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ import styles from './Hero.module.css';
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <h1 className={styles.title}>Powerful for developers. <br></br>Fast for everyone.</h1>
-      <p className={styles.subtitle}>Bring blockchain to the people. Foundation Coin supports experiences for power users, new consumers, and everyone in between.</p>
+      <h1 className={styles.title}>Made For Developers. <br></br>Interplanetary Scale. <br></br>Fast for everyone.</h1>
+      <p className={styles.subtitle}>Time to bring blockchain to the whole solar system. Foundation Coin supports experiences across vast distances, everyday users, and all the rest.</p>
       <div className={styles.ctaContainer}>
         {/* Use ctaButton classes for primary button styles */}
         <button className={`${styles.button} ${styles.ctaButton}`}>Start Building</button>
